bbeb83a86aa2a9916062a25c8c33e16df806383b
Kokecoco <gs2312160@gll-kaisei-s.sapporo-c.ed.jp>
2024-06-18 22:11:09 +0900

    ✨ feat: 新アイコン適用とあといろいろ

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,9 +1,9 @@
 const dayName = ["日", "月", "火", "水", "木", "金", "土"];
 
-function updateClock(){
+function updateClock() {
     const now = new Date();
     const year = now.getFullYear();
-    const month = now.getMonth(); 
+    const month = now.getMonth();
     const date = now.getDate();
     const day = now.getDay();
     const hours = now.getHours();
@@ -11,16 +11,28 @@ function updateClock(){
     const seconds = now.getSeconds();
 
     // パディングを追加
-    let month_formated = ("0" + (month + 1)).slice(-2) + "月";
-    let date_formated = ("0" + date).slice(-2) + "日";
-    let hours_formated = ("0" + hours).slice(-2);
-    let minutes_formated = ("0" + minutes).slice(-2);
-    let seconds_formated = ("0" + seconds).slice(-2);
+    let month_formatted = ("0" + (month + 1)).slice(-2) + "月";
+    let date_formatted = ("0" + date).slice(-2) + "日";
+    let hours_formatted = ("0" + hours).slice(-2);
+    let minutes_formatted = ("0" + minutes).slice(-2);
+    let seconds_formatted = ("0" + seconds).slice(-2);
 
     // 時計の表示を更新
-    document.getElementById('clock').innerHTML = year + "年" + month_formated + date_formated + "(" + dayName[day] + ")" + "<br/>" + hours_formated + ":" + minutes_formated + "." + seconds_formated;
+    document.getElementById("clock").innerHTML =
+        year +
+        "年" +
+        month_formatted +
+        date_formatted +
+        "(" +
+        dayName[day] +
+        ")" +
+        "<br/>" +
+        hours_formatted +
+        ":" +
+        minutes_formatted +
+        "." +
+        seconds_formatted;
 }
-
 
 setInterval(updateClock, 100);
 updateClock();
