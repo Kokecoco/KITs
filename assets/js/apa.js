83259b9c482477f0bd059d6c4f2c9697335d0cb6
YouTuber-repopo <mitchycraft.java@gmail.com>
2024-07-03 17:19:42 +0900

    :bug: fix: APAスタイルフォームリセット時にアクセス日がリセットされないバグを修正

--- assets/js/apa.js
+++ assets/js/apa.js
@@ -1,8 +1,11 @@
-// ページ読み込み時にローカルストレージから履歴取得
+// ページ読み込み時
 window.onload = function () {
+  // ローカルストレージから取得
   document.getElementById('result').value = localStorage.getItem('apa-result') || '';
-}
 
+  // アクセス日を今日に設定
+  setAccessDateDefault();
+}
 
 
 // APAスタイル作成
@@ -20,7 +23,6 @@ function createAPA() {
   else if ($author.value) { authorName = $author.value; }
   else if ($site.value) { authorName = $site.value; }
 
-  // alert(`${authorName}(${publishDate}). ${$page.value}. Retrieved ${accessDate}. from ${$url.value}`);
   printAPAResult(`${authorName}(${publishDate}). ${$page.value}. Retrieved ${accessDate}. from ${$url.value}`);
 
   function printAPAResult(result) {
@@ -29,6 +31,20 @@ function createAPA() {
   }
 }
 
+function setAccessDateDefault() {
+  const monthNames = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  // 現在の日付を取得
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1; // getMonth()は0から始まるため、1を加えます
+  const day = today.getDate();
+
+  // 閲覧日を今日の日付に設定
+  $accessYear.value = year;
+  $accessMonth.value = monthNames[month];
+  $accessDay.value = day;
+}
 
 function resetForms() {
   $author.value = '';
@@ -38,6 +54,7 @@ function resetForms() {
   $publishYear.value = '';
   $publishMonth.value = '';
   $publishDay.value = '';
+  setAccessDateDefault();
 }
 
 
@@ -54,27 +71,10 @@ const $accessMonth = document.getElementById('access-month');
 const $accessDay = document.getElementById('access-day');
 const $result_textarea = document.getElementById('result');
 
-
 document.getElementById('create-button').addEventListener('click', createAPA);
 document.getElementById('reset-button').addEventListener('click', resetForms);
 document.getElementById('textarea-reset-button').addEventListener('click', () => {
   $result_textarea.value = '';
   localStorage.setItem('apa-result', '');
-  console.log('reset!');
 });
 
-// 現在の日付を取得
-const today = new Date();
-
-// 年、月、日を取得
-const year = today.getFullYear();
-const month = today.getMonth() + 1; // getMonth()は0から始まるため、1を加えます
-const day = today.getDate();
-
-// 月の数値を英語の名前に変換する配列
-const monthNames = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-// 閲覧日を今日の日付に設定
-$accessYear.value = year;
-$accessMonth.value = monthNames[month];
-$accessDay.value = day;
